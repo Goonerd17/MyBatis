02d43971058932f1f712d7f4523dd053473ef353
Sihwan Yu <tlghks1217@naver.com>
2024-04-08 22:12:34 +0900

    first commit

---
+++ src/main/java/com/example/goonerd/GoonerdApplication.java
@@ -0,0 +1,13 @@
+package com.example.goonerd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoonerdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoonerdApplication.class, args);
+    }
+
+}
